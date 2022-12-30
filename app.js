08ec1cfb7b13e3283a49f0ec427e39e48edfb7ae
Sivak Hico <strukggg@gmail.com>
2022-12-30 13:51:43 +0100

    new one

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
+import categoriesRoutes from './routes/categories.js'
+import ordersRoutes from './routes/orders.js'
+import productsRoutes from './routes/products.js'
+import usersRoutes from './routes/users.js'
+import cors from 'cors'
+import 'dotenv/config'
+import authJwt from './helpers/jwt.js'
+import errorHandler from './helpers/error-handler.js'
+const PORT = process.env.PORT || 3000
+const api = process.env.API_URL
+const connectDB = process.env.CONNECT_DB
+const app = express()
+import path from 'path'
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
+app.use(cors())
+app.options('*', cors())
+
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(authJwt())
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'))
+app.use(errorHandler)
+
+app.use(`${api}/categories`, categoriesRoutes)
+app.use(`${api}/orders`, ordersRoutes)
+app.use(`${api}/products`, productsRoutes)
+app.use(`${api}/users`, usersRoutes)
+
+mongoose
+    .connect(connectDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'hico-shop'
+    })
+    .then(() => {
+        console.log('DataBase Connection is Ready...')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.listen(PORT, () => {
+    console.log(`The Server is running now on port ${PORT}`)
+})
