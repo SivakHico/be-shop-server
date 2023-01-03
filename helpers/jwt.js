280d64d216241e19c322df4bc76b92bf2b5fc2f8
Sivak Hico <strukggg@gmail.com>
2023-01-03 11:03:47 +0100

    33

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,10 +1,12 @@
 import expressJwt from 'express-jwt'
 
 export default function authJwt() {
+    const secret = process.env.secret
     const api = process.env.API_URL
     return expressJwt({
-        secret: process.env.secret,
+        secret,
         algorithms: ['HS256'],
+        isRevoked: isRevoked
     }).unless({
         path: [
             { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
